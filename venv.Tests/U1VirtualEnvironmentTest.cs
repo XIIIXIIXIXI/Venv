63af00d5cab7a4c3d4fc350e23d582d8eb4f3b11
Koch, Martin <martin.koch@wartsila.com>
2024-02-28 11:58:57 +0100

    Test to source control

---
+++ venv.Tests/U1VirtualEnvironmentTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace venv.Tests
+{
+    internal class U1VirtualEnvironmentTest
+    {
+    }
+}
